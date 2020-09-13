8f6eeba7473a713bb7e650bdc08144952876b8d4
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2020-09-13 09:25:29 +0300

    initial commit

---
+++ application/src/helper.c
@@ -0,0 +1,533 @@
+
+#include <inttypes.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "helper.h"
+
+// in case the user defines USE_ITOA_LUT locally
+//#include "config.h"
+
+//#include "uart0.h"
+
+// |error| < 0.005
+float _atan2f(const float y, const float x)
+{
+
+    float rv;
+    float z = y / x;
+
+    if (x == 0.0f) {
+        if (y > 0.0f) {
+            return PIBY2_FLOAT;
+        }
+        if (y == 0.0f) {
+            return 0.0f;
+        }
+        return -PIBY2_FLOAT;
+    }
+    if (fabsf(z) < 1.0f) {
+        rv = z / (1.0f + 0.28f * z * z);
+        if (x < 0.0f) {
+            if (y < 0.0f) {
+                return rv - PI_FLOAT;
+            }
+            return rv + PI_FLOAT;
+        }
+    } else {
+        rv = PIBY2_FLOAT - z / (z * z + 0.28f);
+        if (y < 0.0f) {
+            return rv - PI_FLOAT;
+        }
+    }
+    return rv;
+}
+
+float _sin(const float x)
+{
+    unsigned char i;
+    float denum = 1;
+    float res = 0;
+    float x_2 = x * x;
+    float num = x;
+    int s = 1;
+    for (i = 0; i < PREC; i++) {
+        res += s * (num / denum);
+        denum = denum * (denum + 1) * (denum + 2);
+        num = num * x_2;
+        s = s * -1;
+    }
+    return res;
+    /*
+       float x_3 = x * x_2;
+       float x_5 = x_3 * x_2;
+       float x_7 = x_5 * x_2;
+       float res = (x - x_3/6.0 + x_5/120.0 - x_7/5040.0 );
+       return res;//+ x_9/362880.0); */
+}
+
+float _cos(const float x)
+{
+    unsigned char i;
+    float denum = 2;
+    float res = 1;
+    float x_2 = x * x;
+    float num = x_2;
+    int s = -1;
+    for (i = 0; i < PREC; i++) {
+        res += s * (num / denum);
+        denum = denum * (denum + 1) * (denum + 2);
+        num = num * x_2;
+        s = s * -1;
+    }
+    return res;
+    /*
+
+       float x_2 = x * x;
+       float x_4 = x_2 * x_2;
+       float x_6 = x_4 * x_2;
+       float res = 1 - x_2 / 2.0 + x_4 / 24.0 - x_6 / 720.0 ;
+       //float x_8 = x_6 * x_2;
+       return res ;//+ x_8/40320.0;
+     */
+}
+
+float _sqrt(const float number)
+{
+    unsigned char i = 0;
+    float x0, sqx0, error;
+    if (number < 1) {
+        x0 = number * 2;
+    } else {
+        x0 = number / 2;
+    }
+    do {
+        x0 = (x0 + (number / x0)) / 2;
+        sqx0 = x0 * x0;
+        error = (number - sqx0) / number;
+        i++;
+    } while (i < 20
+             && ((error > 0 && error > 0.01) || (error < 0 && error < -0.01)));
+
+    return x0;
+}
+
+float radians(const float x)
+{
+    return PI * x / 180.0f;
+}
+
+float sq(const float x)
+{
+    return x * x;
+}
+
+// ###############################################
+// #
+// #  time functions
+// #
+uint32_t get_unixtime(struct ts t)
+{
+    const uint8_t days_in_month[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    uint8_t i;
+    uint16_t d;
+    int16_t y;
+    uint32_t rv;
+
+    if (t.year >= 2000) {
+        y = t.year - 2000;
+    } else {
+        return 0;
+    }
+
+    d = t.mday - 1;
+    for (i=1; i<t.mon; i++) {
+        d += (uint16_t) (days_in_month[i - 1]);
+    }
+    if (t.mon > 2 && y % 4 == 0) {
+        d++;
+    }
+    // count leap days
+    d += (365 * y + (y + 3) / 4);
+    rv = ((d * 24UL + t.hour) * 60 + t.min) * 60 + t.sec + SECONDS_FROM_1970_TO_2000;
+    return rv;
+}
+
+
+// ###############################################
+// #
+// #  string functions
+// #
+
+uint8_t str_to_uint8(char *str, uint8_t * out, const uint8_t seek,
+                      const uint8_t len, const uint8_t min, const uint8_t max)
+{
+    uint32_t val = 0, pow = 1;
+    uint8_t i;
+
+    // pow() is missing in msp gcc, so we improvise
+    for (i = 0; i < len - 1; i++) {
+        pow *= 10;
+    }
+    for (i = 0; i < len; i++) {
+        if ((str[seek + i] > 47) && (str[seek + i] < 58)) {
+            val += (str[seek + i] - 48) * pow;
+        }
+        pow /= 10;
+    }
+    if ((val >= min) && (val <= max)) {
+        *out = val;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
+
+uint8_t str_to_uint16(char *str, uint16_t *out, const uint8_t seek,
+                      const uint8_t len, const uint16_t min, const uint16_t max)
+{
+    uint16_t val = 0;
+    uint32_t pow = 1;
+    uint8_t i, c;
+
+    for (i = len + 1; i > seek; i--) {
+        c = str[i-1] - 48;
+        if (c < 10) {
+            val += c * pow;
+            pow *= 10;
+        } else {
+            if (val) {
+                // if we already have a number and this char is unexpected
+                break;
+            }
+        }
+    }
+
+    if ((val >= min) && (val <= max)) {
+        *out = val;
+    } else {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+uint8_t str_to_uint32(char *str, uint32_t *out, const uint8_t seek,
+                      const uint8_t len, const uint32_t min, const uint32_t max)
+{
+    uint32_t val = 0, pow = 1;
+    uint8_t i;
+
+    // pow() is missing in msp gcc, so we improvise
+    for (i = 0; i < len - 1; i++) {
+        pow *= 10;
+    }
+    for (i = 0; i < len; i++) {
+        if ((str[seek + i] > 47) && (str[seek + i] < 58)) {
+            val += (str[seek + i] - 48) * pow;
+        }
+        pow /= 10;
+    }
+    if ((val >= min) && (val <= max)) {
+        *out = val;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
+
+uint8_t str_to_int32(char *str, int32_t *out, const uint8_t seek,
+                      const uint8_t len, const int32_t min, const int32_t max)
+{
+    int32_t val = 0;
+    uint32_t pow = 1;
+    uint8_t i;
+    int8_t sign = 1;
+
+    // pow() is missing in msp gcc, so we improvise
+    for (i = 0; i < len - 1; i++) {
+        pow *= 10;
+    }
+    for (i = 0; i < len; i++) {
+        if ((str[seek + i] > 47) && (str[seek + i] < 58)) {
+            val += (str[seek + i] - 48) * pow;
+        } else if (str[seek + i] == 45) {
+            sign = -1;
+        }
+        pow /= 10;
+    }
+    if ((val >= min) && (val <= max)) {
+        *out = val * sign;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
+
+uint8_t hstr_to_uint8(char *str, uint8_t *out, const uint8_t seek,
+                      const uint8_t len, const uint8_t min, const uint8_t max)
+{
+    uint8_t val = 0;
+    uint32_t pow = 1;
+    uint8_t i;
+
+    // pow() is missing in msp gcc, so we improvise
+    for (i = seek; i < len; i++) {
+        pow *= 16;
+    }
+
+    for (i = seek; i < len + 1; i++) {
+        if ((str[i] > 47) && (str[i] < 58)) {
+            // 0 - 9
+            val += (str[i] - 48) * pow;
+        } else if ((str[i] > 96) && (str[i] < 103)) {
+            // a - f
+            val += (str[i] - 87) * pow;
+        } else if ((str[i] > 64) && (str[i] < 71)) {
+            // A - F
+            val += (str[i] - 55) * pow;
+        }
+        pow /= 16;
+    }
+
+    if ((val >= min) && (val <= max)) {
+        *out = val;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
+
+uint8_t hstr_to_uint16(char *str, uint16_t *out, const uint8_t seek,
+                      const uint8_t len, const uint16_t min, const uint16_t max)
+{
+    uint16_t val = 0;
+    uint32_t pow = 1;
+    uint8_t i;
+
+    // pow() is missing in msp gcc, so we improvise
+    for (i = seek; i < len; i++) {
+        pow *= 16;
+    }
+
+    for (i = seek; i < len + 1; i++) {
+        if ((str[i] > 47) && (str[i] < 58)) {
+            // 0 - 9
+            val += (str[i] - 48) * pow;
+        } else if ((str[i] > 96) && (str[i] < 103)) {
+            // a - f
+            val += (str[i] - 87) * pow;
+        } else if ((str[i] > 64) && (str[i] < 71)) {
+            // A - F
+            val += (str[i] - 55) * pow;
+        }
+        pow /= 16;
+    }
+
+    if ((val >= min) && (val <= max)) {
+        *out = val;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
+
+
+static uint16_t const hex_ascii[16] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66 };
+
+char *_utoh(char *buf, const uint32_t val)
+{
+    char *p = buf + CONV_BASE_8_BUF_SZ - 1; // the very end of the buffer
+    uint32_t m = val;
+    uint8_t i = 0;
+
+    *p = '\0';
+
+    if (val == 0) {
+        p -= 1;
+        memcpy(p, &hex_ascii[0], sizeof(uint8_t));
+    }
+
+    // groups of 8 bits
+    while (m > 0 || (i & 1))
+    {
+        p -= 1;
+        memcpy(p, &hex_ascii[m & 0xf], sizeof(uint8_t));
+        m >>= 4;
+        i++;
+    }
+
+    p -= 2;
+    memcpy(p, "0x" , sizeof(uint16_t));
+
+    return p;
+}
+
+#ifdef USE_ITOA_LUT
+
+static uint16_t const dec_ascii[10] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 };
+static uint16_t const bin_ascii[2] = { 0x30, 0x31 };
+
+char *_uint32toa(char *buf, const uint32_t val)
+{
+    char *p = buf + CONV_BASE_10_BUF_SZ - 1; // the very end of the buffer
+    uint32_t m = val;
+
+    *p = '\0';
+
+    while(m >= 10)
+    {
+        uint32_t const old = m;
+
+        p -= 1;
+        m /= 10;
+        memcpy(p, &dec_ascii[old - (m * 10)], sizeof(uint8_t));
+    }
+
+    p -= 1;
+    memcpy(p, &dec_ascii[m], sizeof(uint8_t));
+
+    return p;
+}
+
+char *_utob(char *buf, const uint16_t val)
+{
+    char *p = buf + CONV_BASE_2_BUF_SZ - 1; // the very end of the buffer
+    uint16_t m = val;
+    uint8_t i = 0;
+
+    *p = '\0';
+
+    if (val == 0) {
+        p -= 1;
+        memcpy(p, &bin_ascii[0], sizeof(uint8_t));
+    }
+
+    // groups of 8bits
+    while (m > 0 || (i & 7))
+    {
+        if (m > 0 && !(i & 7)) {
+            p -= 1;
+            *p = ' ';
+        }
+        p -= 1;
+        memcpy(p, &bin_ascii[m & 0x1], sizeof(uint8_t));
+        m >>= 1;
+        i++;
+    }
+
+    return p;
+}
+#else
+
+char *_uint32toa(char *buf, const uint32_t val)
+{
+    char *p = buf + CONV_BASE_10_BUF_SZ - 1; // the very end of the buffer
+    uint32_t m = val;
+
+    *p = '\0';
+
+    if (val == 0) {
+        p -= 1;
+        *p = '0';
+    }
+
+    while (m > 0) {
+        p -= 1;
+        *p = (m % 10) + '0';
+        m /= 10;
+    }
+
+    return p;
+}
+
+char *_utob(char *buf, const uint16_t val)
+{
+    char *p = buf + CONV_BASE_2_BUF_SZ - 1; // the very end of the buffer
+    uint16_t m = val;
+    uint8_t i = 0;
+
+    *p = '\0';
+
+    if (val == 0) {
+        p -= 1;
+        *p = '0';
+    }
+
+    // groups of 8bits
+    while (m > 0 || (i & 7))
+    {
+        if (m > 0 && !(i & 7)) {
+            p -= 1;
+            *p = ' ';
+        }
+        p -= 1;
+        *p = (m & 0x1) + '0' ;
+        m >>= 1;
+        i++;
+    }
+
+    return p;
+}
+
+#endif
+
+char *_utoa(char *buf, const uint32_t val)
+{
+    return _uint32toa(buf, val);
+}
+
+char *_itoa(char *buf, const int32_t val)
+{
+    char *p;
+    if (val >= 0) {
+        return _uint32toa(buf, val);
+    } else {
+        p = _uint32toa(buf, val * -1);
+        *(p - 1) = '-';
+        return p-1;
+    }
+}
+
+char *prepend_padding(char *buf, char *converted_buf, const pad_type padding_type, const uint8_t target_len)
+{
+    uint8_t conv_len;
+    uint8_t buf_pos;
+    uint8_t cnt;
+    uint8_t padding_char = '0';
+
+    conv_len = strlen(converted_buf);
+    buf_pos = converted_buf - buf;
+
+    // if not enough buffer space is available for the prepend
+    if (buf_pos < target_len-conv_len) {
+        return converted_buf;
+    }
+
+    // if the converted string is already longer than the target length
+    if (target_len <= conv_len) {
+        return converted_buf;
+    } 
+
+    if (padding_type == PAD_SPACES) {
+        padding_char = ' ';
+    }
+
+    for (cnt=0; cnt<target_len-conv_len; cnt++) {
+        *(buf + buf_pos - cnt - 1) = padding_char;
+    }
+
+    return buf + buf_pos - cnt;
+}
+
+uint8_t dec_to_bcd(const uint8_t val)
+{
+    return ((val / 10 * 16) + (val % 10));
+}
+
+uint8_t bcd_to_dec(const uint8_t val)
+{
+    return ((val / 16 * 10) + (val % 16));
+}
+
---
+++ application/src/pg.c
@@ -0,0 +1,147 @@
+
+#include <inttypes.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "helper.h"
+#include "tlpi_hdr.h"
+
+#define          BUF_SIZE  1024000
+#define  BLOCK_SIZE_1BYTE  1
+#define BLOCK_SIZE_2BYTES  2
+
+// cp from infile to outfile and detect holes
+int main(int argc, char *argv[])
+{
+    int fdin, fdout;
+    char *infile = NULL;
+    char *outfile = NULL;
+    int opt;
+    char buf[BUF_SIZE];
+    ssize_t cnt;
+    //ssize_t seek_cnt = 0;
+    uint64_t seq_cnt = 1;
+    uint64_t i;
+    uint16_t ccl; // current adc conversion level
+    uint8_t oll; // old logic level
+    uint8_t cll; // current logic level
+    uint64_t test = 1;
+    uint8_t channel;
+    uint8_t block_size = BLOCK_SIZE_2BYTES;
+
+    while ((opt = getopt(argc, argv, "b:i:o:c:")) != -1) {
+        switch (opt) {
+        case 'b':
+            hstr_to_uint8(optarg, &block_size, 0, strlen(optarg) - 1, 1, 2);
+            break;
+        case 'c':
+            hstr_to_uint8(optarg, &channel, 0, strlen(optarg) - 1, 0, 16);
+            break;
+        case 'i':
+            infile = optarg;
+            break;
+        case 'o':
+            outfile = optarg;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if ((fdin = open(infile, O_RDONLY)) < 0) {
+        errExit("opening input file");
+    }
+
+    if ((fdout = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR)) < 0) {
+        errExit("opening output file");
+    }
+
+    // initialize readings
+    switch (block_size) {
+        case BLOCK_SIZE_2BYTES:
+            read(fdin, buf, BLOCK_SIZE_2BYTES);
+            ccl = (buf[0] + (buf[1] << 8)) & channel;
+            if (ccl) {
+                oll = 1;
+            } else {
+                oll = 0;
+            }
+            break;
+
+        case BLOCK_SIZE_1BYTE:
+            read(fdin, buf, BLOCK_SIZE_1BYTE);
+            ccl = buf[0] & channel;
+            if (ccl) {
+                oll = 1;
+            } else {
+                oll = 0;
+            }
+            break;
+        default:
+            errExit("unsuported block size");
+            break;
+    }
+
+    // read all values
+    switch (block_size) {
+
+        case BLOCK_SIZE_2BYTES:
+            while ((cnt = read(fdin, buf, BUF_SIZE)) > 0) {
+
+                for (i=0;i<cnt;i+=2) {
+                    // get current sample point
+                    ccl = (buf[i] + (buf[i+1] << 8)) & channel;
+                    if (ccl) {
+                        cll = 1;
+                        test++;
+                    } else {
+                        cll = 0;
+                        test++;
+                    }
+    
+                    if (cll != oll) {
+                        printf ("%d %lu\r\n", oll, seq_cnt);
+                        oll = cll;
+                        seq_cnt = 1;
+                    } else {
+                        seq_cnt++;
+                    }
+                }
+            }
+            break;
+        case BLOCK_SIZE_1BYTE:
+            while ((cnt = read(fdin, buf, BUF_SIZE)) > 0) {
+
+                for (i=0;i<cnt;i++) {
+                    // get current sample point
+                    ccl = buf[i] & channel;
+                    if (ccl) {
+                        cll = 1;
+                        test++;
+                    } else {
+                        cll = 0;
+                        test++;
+                    }
+    
+                    if (cll != oll) {
+                        printf ("%d %lu\r\n", oll, seq_cnt);
+                        oll = cll;
+                        seq_cnt = 1;
+                    } else {
+                        seq_cnt++;
+                    }
+                }
+            }
+            break;
+        }
+
+    printf ("%d %lu\r\n", cll, seq_cnt);
+
+    close(fdin);
+    close(fdout);
+
+    printf("test is %lu\r\n", test);
+}
+
+
