ef93d8bdd2303fba18c20cd7ff4d7b1272d6f331
Petre Rodan <2b4eda@subdimension.ro>
2021-10-05 10:33:24 +0000

    cleanup

--- software/version.h
+++ software/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Mon Sep 27 16:54:49 UTC 2021
+// compiled on Tue Oct  5 10:29:44 UTC 2021
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 22
-#define COMMIT 40
+#define BUILD 24
+#define COMMIT 42
 
 #endif
