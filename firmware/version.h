d65ba08f87c97af1e20ffeaf3d5b684b73dcbcfb
Petre Rodan <2b4eda@subdimension.ro>
2021-10-29 18:22:15 +0000

    add more test signals

--- firmware/version.h
+++ firmware/version.h
@@ -1,13 +1,13 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Fri Oct 29 17:36:54 UTC 2021
+// compiled on Fri Oct 29 18:14:04 UTC 2021
 
 #define VER_REPLAY  1
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 681
-#define COMMIT 81
+#define BUILD 683
+#define COMMIT 82
 
 #endif
