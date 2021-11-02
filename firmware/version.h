9bfc06034e30bafabfac9add5a08410a88fdcb11
Petre Rodan <2b4eda@subdimension.ro>
2021-11-02 10:58:08 +0200

    properly include FAMILY glue functions into tags

--- firmware/version.h
+++ firmware/version.h
@@ -1,13 +1,13 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sun Oct 31 10:21:09 UTC 2021
+// compiled on Tue Nov  2 08:32:12 UTC 2021
 
 #define VER_REPLAY  1
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 697
-#define COMMIT 85
+#define BUILD 698
+#define COMMIT 87
 
 #endif
