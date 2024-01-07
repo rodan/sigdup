62767181b05a2e6d05d99fba80c0dbeb78d7294c
Petre Rodan <petre.rodan@subdimension.ro>
2024-01-07 21:42:59 +0200

    Makefile fixes

--- firmware/version.h
+++ firmware/version.h
@@ -1,13 +1,13 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Wed Aug 10 10:06:29 UTC 2022
+// compiled on Mon Jan 30 04:18:01 UTC 2023
 
 #define VER_REPLAY  1
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 739
-#define COMMIT 101
+#define BUILD 740
+#define COMMIT 102
 
 #endif
